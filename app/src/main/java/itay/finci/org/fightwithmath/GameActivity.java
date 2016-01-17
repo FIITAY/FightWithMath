9aedd186fa21c1194f972d7871f3626528e0cf5d
Itay <itay@finci.org>
2016-01-17 21:32:00 +0200

    Gameactivity updat:
    gameactivity update:
    -added timer
    -added solve button
    
    timer mission removed.
    next mission is:
    -write awnswer area taht take information
    - text filed of the math problem

--- app/src/main/java/itay/finci/org/fightwithmath/GameActivity.java
+++ app/src/main/java/itay/finci/org/fightwithmath/GameActivity.java
@@ -1,13 +1,29 @@
 package itay.finci.org.fightwithmath;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class GameActivity extends AppCompatActivity {
+    CountDownTimer coutdown;
+    TextView timerView;
+    long timersec;
+    Button bSolve;
+    int colorred;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,35 @@ public class GameActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        timerView = (TextView) findViewById(R.id.timer1);
+        coutdown = new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timerView.setText(""+millisUntilFinished / 1000);
+                timersec = millisUntilFinished;
+                if (millisUntilFinished/1000 <= 10){
+                    timerView.setTextColor(Color.parseColor("#FF0808"));
+                }
+            }
+
+            public void onFinish() {
+                timerView.setText(R.string.timeout);
+            }
+        };
+        coutdown.start();
+
+        bSolve= (Button) findViewById(R.id.bSolve);
+        bSolve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                coutdown.cancel();
+                Context context = getApplicationContext();
+                CharSequence text = getString(R.string.finishtimepart1)+ " "+ timersec /1000 +" "+ getString(R.string.finishtimepart2);
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 
 }
--- app/src/main/java/itay/finci/org/fightwithmath/GameActivityFragment.java
+++ app/src/main/java/itay/finci/org/fightwithmath/GameActivityFragment.java
@@ -1,22 +1,29 @@
 package itay.finci.org.fightwithmath;
 
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class GameActivityFragment extends Fragment {
-
+    private TextView timerView;
+    private long timersec;
+    private CountDownTimer coutdown;
     public GameActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        View view = inflater.inflate(R.layout.fragment_game, container, false);
+        return view;
     }
 }
