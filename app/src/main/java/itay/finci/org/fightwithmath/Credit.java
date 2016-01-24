030256509654281bfd23322b695c21ed33b00d6d
Ilan Finci <Ilan.Finci@arm.com>
2016-01-24 16:54:55 +0200

    Added version name at the bottom of Credit screen

--- app/src/main/java/itay/finci/org/fightwithmath/Credit.java
+++ app/src/main/java/itay/finci/org/fightwithmath/Credit.java
@@ -1,11 +1,13 @@
 package itay.finci.org.fightwithmath;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class Credit extends AppCompatActivity {
 
@@ -17,6 +19,12 @@ public class Credit extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView t = (TextView) findViewById(R.id.tVersion);
+        try {
+            t.setText(getPackageManager().getPackageInfo(getPackageName(), 0).versionName);
+        }catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
